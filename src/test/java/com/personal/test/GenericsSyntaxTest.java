a5b8ee0531b550600377316616bb69068e3f9329
Piyush Roy <piyush2k13@gmail.com>
2014-02-25 22:29:18 +0530

    Added generic class and it syntax

---
+++ src/test/java/com/personal/test/GenericsSyntaxTest.java
@@ -0,0 +1,64 @@
+package com.personal.test;
+
+class MyGenericClass<T>{
+	
+	/**
+	 * Generic is specified at class level
+	 */
+	private T memberVariable;
+	
+	public T myMethod(T t ) {
+		return t;
+	}
+	
+	/**
+	 * Another method where generic is not specified in class level
+	 */
+	public <E> E myAnotherMethod() {
+		return null;
+	}
+
+	public T getMemberVariable() {
+		return memberVariable;
+	}
+
+	public void setMemberVariable(T memberVariable) {
+		this.memberVariable = memberVariable;
+	}
+	
+}
+
+class AnotheGenericClass<R extends MyGenericClass<T>, T> {
+	
+	private R memberVariable;
+	
+	/**
+	 * Only generic R that extends super class can call its method
+	 * @param r
+	 */
+	public void myMethod(R r) {
+		 
+		if(r != null)
+		{
+			r.myAnotherMethod();
+		}
+		
+	}
+
+	public R getMemberVariable() {
+		return memberVariable;
+	}
+
+	public void setMemberVariable(R memberVariable) {
+		this.memberVariable = memberVariable;
+	}
+}
+
+public class GenericsSyntaxTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
