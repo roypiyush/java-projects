3c5adbe23c82ad24a0172cf510182113731103a7
Piyush Roy <piyush2k13@gmail.com>
2014-11-15 23:40:12 +0530

    Formated sql code for better readability

--- src/main/resources/mysql/Employee.sql
+++ src/main/resources/mysql/Employee.sql
@@ -5,11 +5,34 @@
 create database EMPLOYEE;
 use EMPLOYEE;
 
-CREATE TABLE `Name` (`NameKey` INT NOT NULL AUTO_INCREMENT, `FirstName` VARCHAR(30), `MiddleName` VARCHAR(30), `LastName` VARCHAR(30), PRIMARY KEY(`NameKey`)) ENGINE=INNODB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
+CREATE TABLE `Name` 
+(
+	`NameKey` INT NOT NULL AUTO_INCREMENT, 
+	`FirstName` VARCHAR(30), 
+	`MiddleName` VARCHAR(30), 
+	`LastName` VARCHAR(30), 
+	PRIMARY KEY(`NameKey`)
+) ENGINE=INNODB DEFAULT CHARSET = utf8 AUTO_INCREMENT = 1;
 
-CREATE TABLE `Company` (`CompanyKey` INT NOT NULL AUTO_INCREMENT, `CompanyName` VARCHAR(40), PRIMARY KEY(`CompanyKey`)) ENGINE=INNODB DEFAULT CHARSET = UTF8 AUTO_INCREMENT=1;
+CREATE TABLE `Company` 
+(
+	`CompanyKey` INT NOT NULL AUTO_INCREMENT, 
+	`CompanyName` VARCHAR(40), 
+	PRIMARY KEY(`CompanyKey`)
+) ENGINE=INNODB DEFAULT CHARSET = UTF8 AUTO_INCREMENT=1;
 
-CREATE  TABLE `Employment` (`EmploymentKey` INT NOT NULL AUTO_INCREMENT, `EmployeeId` VARCHAR(30) NOT NULL , `NameKey` INT NOT NULL, `CompanyKey` INT NOT NULL, PRIMARY KEY (`EmploymentKey`), INDEX `fk_NameKey_idx` (`NameKey` ASC), INDEX `fk_CompanyKey_idx` (`CompanyKey` ASC), CONSTRAINT `fk_NameKey` FOREIGN KEY (`NameKey` ) REFERENCES `Name` (`NameKey`) ON DELETE RESTRICT ON UPDATE CASCADE, CONSTRAINT `fk_CompanyKey` FOREIGN KEY (`CompanyKey` ) REFERENCES `Company` (`CompanyKey`) ON DELETE RESTRICT ON UPDATE CASCADE);
+CREATE TABLE `Employment` 
+(
+	`EmploymentKey` INT NOT NULL AUTO_INCREMENT, 
+	`EmployeeId` VARCHAR(30) NOT NULL , 
+	`NameKey` INT NOT NULL, 
+	`CompanyKey` INT NOT NULL, 
+	PRIMARY KEY (`EmploymentKey`), 
+	INDEX `fk_NameKey_idx` (`NameKey` ASC), 
+	INDEX `fk_CompanyKey_idx` (`CompanyKey` ASC), 
+	CONSTRAINT `fk_NameKey` FOREIGN KEY (`NameKey` ) REFERENCES `Name` (`NameKey`) ON DELETE RESTRICT ON UPDATE CASCADE, 
+	CONSTRAINT `fk_CompanyKey` FOREIGN KEY (`CompanyKey` ) REFERENCES `Company` (`CompanyKey`) ON DELETE RESTRICT ON UPDATE CASCADE
+);
 
 INSERT INTO `Name`(`FirstName`,`MiddleName`,`LastName`) VALUES
 ('Name1','Middle1','Last1'),
