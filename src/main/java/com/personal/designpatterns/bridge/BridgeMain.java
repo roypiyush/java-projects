1ebe35da9e77dcf6f3ba92f509765ddee329107c
Piyush Roy <piyush2k13@gmail.com>
2014-11-17 19:17:40 +0530

    Bridge pattern with multi operation at same time.

--- src/main/java/com/personal/designpatterns/bridge/BridgeMain.java
+++ src/main/java/com/personal/designpatterns/bridge/BridgeMain.java
@@ -2,16 +2,42 @@ package com.personal.designpatterns.bridge;
 
 public class BridgeMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-		DrawingApi circleApi = new CircleDrawer();
-		DrawingApi rectangleApi = new RectangleDrawer();
+		final DrawingApi circleApi = new CircleDrawer();
+		final DrawingApi rectangleApi = new RectangleDrawer();
 		
-		ShapeCreator creator = new ConcreteShaperCreator(circleApi);
-		creator.createShape();
+		Thread t1 = new Thread() {
+
+			@Override
+			public void run() {
+				super.run(); // Thread is actually started at this point.
+				ShapeCreator creator = new ConcreteShaperCreator(circleApi);
+				creator.createShape();
+			}
+			
+		};
 		
-		creator = new ConcreteShaperCreator(rectangleApi);
-		creator.createShape();
+		Thread t2 = new Thread() {
+
+			@Override
+			public void run() {
+				super.run(); // Thread is actually started at this point.
+				ShapeCreator creator = new ConcreteShaperCreator(rectangleApi);
+				creator.createShape();
+			}
+			
+		};
+		
+		t1.start();
+		t2.start();
+		
+		System.out.println("Started Inner Threads");
+		t1.join();
+		t2.join();
+		System.out.println("Inner Threads Completed Execution. Existing Main thread.");
+		
+		
 	}
 
 }
