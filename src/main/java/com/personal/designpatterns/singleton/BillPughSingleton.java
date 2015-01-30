598b597429d9717cacbe12cc609e7bec4283775c
Piyush Roy <piyush2k13@gmail.com>
2015-01-30 09:41:41 +0530

    Added Bill Pugh Singleton Example

---
+++ src/main/java/com/personal/designpatterns/singleton/BillPughSingleton.java
@@ -0,0 +1,29 @@
+package com.personal.designpatterns.singleton;
+
+public class BillPughSingleton {
+
+	private BillPughSingleton() {}
+	
+	private static class SingletonHelper {
+		private static final BillPughSingleton SINGLETON = new BillPughSingleton(); 
+	}
+	
+	public static BillPughSingleton getInstance() {
+		return SingletonHelper.SINGLETON;
+	}
+	
+	public static void main(String[] args) {
+		
+		BillPughSingleton instance1 = BillPughSingleton.getInstance();
+		BillPughSingleton instance2 = BillPughSingleton.getInstance();
+		
+		if(instance1.hashCode() == instance2.hashCode()) {
+			System.out.println("Hey, instances are same.");
+		}
+		else {
+			System.out.println("Instances are NOT same.");
+		}
+		
+	}
+
+}
