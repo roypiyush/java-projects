5192abffebbb593a6558f2cebc1980afebc90bde
Piyush Roy <piyush2k13@gmail.com>
2015-03-04 11:29:27 +0530

    Added iterator implementation using enhanced for loop

---
+++ src/main/java/com/personal/tricks/SimpleIteratorMain.java
@@ -0,0 +1,46 @@
+package com.personal.tricks;
+
+import java.util.Iterator;
+
+class SimpleIterator implements Iterator<Object> {
+
+	private int counter = 0;
+	
+	@Override
+	public boolean hasNext() {
+		System.out.println("hasNext called");
+		return (counter < 10) ? true : false;
+	}
+
+	@Override
+	public Object next() {
+		counter++;
+		System.out.print("inside next() " + counter + " ");
+		return getObject();
+	}
+
+	private Object getObject() {
+		return new Object();
+	}
+
+}
+
+class HolderCollection<E> implements Iterable<Object> {
+	private SimpleIterator iterator = new SimpleIterator();
+	
+	public Iterator<Object> iterator() {
+		return iterator;
+	}
+}
+
+public class SimpleIteratorMain {
+	
+	public static void main(String[] args) {
+		HolderCollection<Object> holder = new HolderCollection<Object>();
+		for (Object o : holder) {
+			System.out.println(o.hashCode());
+		}
+	}
+
+}
+
