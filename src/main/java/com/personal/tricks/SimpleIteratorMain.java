e4bff86d1b544892c43093f91e30d1edc55ed5c9
Piyush Roy <piyush2k13@gmail.com>
2015-12-29 09:22:38 +0530

    improved simple iterator

--- src/main/java/com/personal/tricks/SimpleIteratorMain.java
+++ src/main/java/com/personal/tricks/SimpleIteratorMain.java
@@ -2,37 +2,28 @@ package com.personal.tricks;
 
 import java.util.Iterator;
 
-class SimpleIterator implements Iterator<Object> {
-
-	private int counter = 0;
+class HolderCollection<E> implements Iterable<Object> {
 	
-	@Override
-	public boolean hasNext() {
-		System.out.println("hasNext called");
-		return (counter < 10) ? true : false;
-	}
+	private Iterator<Object> iterator = new Iterator<Object>() {
+		private int counter = 0;
 
-	@Override
-	public Object next() {
-		counter++;
-		System.out.print("inside next() " + counter + " ");
-		return getObject();
-	}
+		@Override
+		public boolean hasNext() {
+			System.out.println("hasNext called");
+			return (counter < 10) ? true : false;
+		}
 
+		@Override
+		public Object next() {
+			counter++;
+			System.out.print("inside next() " + counter + " ");
+			return getObject();
+		}
+	};
+	
 	private Object getObject() {
 		return new Object();
 	}
-
-	@Override
-	public void remove() {
-		// TODO Auto-generated method stub
-		
-	}
-
-}
-
-class HolderCollection<E> implements Iterable<Object> {
-	private SimpleIterator iterator = new SimpleIterator();
 	
 	public Iterator<Object> iterator() {
 		return iterator;
