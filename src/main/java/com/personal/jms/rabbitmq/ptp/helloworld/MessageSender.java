4ba567e1f183dc139a0231fa8b1f9fc2175bca5d
Piyush Roy <piyush2k13@gmail.com>
2016-01-04 09:49:22 +0530

    added message as parameter

--- src/main/java/com/personal/jms/rabbitmq/ptp/helloworld/MessageSender.java
+++ src/main/java/com/personal/jms/rabbitmq/ptp/helloworld/MessageSender.java
@@ -7,10 +7,9 @@ import com.rabbitmq.client.Channel;
 
 public class MessageSender {
 
-	public void sendMessage(Channel channel, String queueName) throws IOException, TimeoutException {
+	public void sendMessage(Channel channel, String queueName, String message) throws IOException, TimeoutException {
 		
 	    channel.queueDeclare(queueName, false, false, false, null);
-	    String message = "Hello World!";
 	    channel.basicPublish("", queueName, null, message.getBytes());
 	    System.out.println(" [x] Sent '" + message + "'");
 	    
