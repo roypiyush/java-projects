f330af6c9dfa1790493c1692f656deebdc3d5bc5
Piyush Roy <piyush2k13@gmail.com>
2017-10-14 22:50:48 +0530

    added multiple client call - load test

--- src/main/java/com/personal/thrift/MultiplicationSyncClient.java
+++ src/main/java/com/personal/thrift/MultiplicationSyncClient.java
@@ -10,34 +10,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MultiplicationSyncClient {
+
     public static void main(String[] args) {
 
+        class RunnableJob implements Runnable {
+            private int id;
 
-        TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 9090));
-        TProtocol protocol = new TCompactProtocol.Factory().getProtocol(transport);
+            public RunnableJob (int id) {
+                this.id = id;
+            }
 
-
-        final List<Thread> threads = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-
-            Thread t = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        MultiplicationService.Client client = new MultiplicationService.Client(protocol);
-                        transport.open();
-                        int result = client.multiply(threads.size() + 1, 5);
-                        System.out.println(String.format("result=%d", result));
-                        transport.close();
-                    } catch (TException e) {
-                        e.printStackTrace();
-                    }
+            public void run() {
+                try {
+                    TFramedTransport transport = new TFramedTransport(new TSocket("localhost", 9090));
+                    TProtocol protocol = new TCompactProtocol.Factory().getProtocol(transport);
+                    MultiplicationService.Client client = new MultiplicationService.Client(protocol);
+                    transport.open();
+                    int result = client.multiply(id + 1, 5);
+                    System.out.println(String.format("result=%d", result));
+                    transport.close();
+                } catch (TException e) {
+                    e.printStackTrace();
                 }
-            });
-            threads.add(t);
+            }
         }
-        for (int i = 0; i < 100; i++) {
-            threads.get(i).start();
+
+        final List<RunnableJob> runnables = new ArrayList<>();
+        int thread_size = 100000;
+        for (int i = 0; i < thread_size; i++) {
+            runnables.add(new RunnableJob(i));
+        }
+        for (int i = 0; i < thread_size; i++) {
+            new Thread(runnables.get(i)).start();
         }
 
 
