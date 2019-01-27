0754b6e018a28a0ce51f0faa6e9bbc166cf033c1
Piyush <piyush@email.com>
2019-01-27 10:44:37 +0530

    refactored word count

--- src/main/java/com/personal/bigdata/hadoop/mapred/WordCount.java
+++ src/main/java/com/personal/bigdata/hadoop/mapred/WordCount.java
@@ -4,35 +4,22 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapred.*;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
 
-/**
- * Created by piyushr on 10/26/17.
- */
-public class WordCountv1_0 {
+public class WordCount {
 
     public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
         private Text word = new Text();
 
-        @Override
         public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(line);
-            while (tokenizer.hasMoreElements()) {
+            while (tokenizer.hasMoreTokens()) {
                 word.set(tokenizer.nextToken());
                 output.collect(word, one);
             }
@@ -40,8 +27,6 @@ public class WordCountv1_0 {
     }
 
     public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
-
-        @Override
         public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
             int sum = 0;
             while (values.hasNext()) {
@@ -52,8 +37,8 @@ public class WordCountv1_0 {
     }
 
     public static void main(String[] args) throws Exception {
-        JobConf conf = new JobConf(WordCountv1_0.class);
-        conf.setJobName("WordCount");
+        JobConf conf = new JobConf(WordCount.class);
+        conf.setJobName("wordcount");
 
         conf.setOutputKeyClass(Text.class);
         conf.setOutputValueClass(IntWritable.class);
@@ -65,8 +50,11 @@ public class WordCountv1_0 {
         conf.setInputFormat(TextInputFormat.class);
         conf.setOutputFormat(TextOutputFormat.class);
 
-        FileInputFormat.setInputPaths(conf, new Path(WordCountv1_0.class.getClassLoader().getResource("input.txt").getPath()));
-        FileOutputFormat.setOutputPath(conf, new Path("target/wordCountOutput.txt"));
-    }
+        String source = WordCount.class.getClassLoader().getResource("input.txt").getPath();
+        FileInputFormat.setInputPaths(conf, new Path(source));
+        String destination = System.getProperty("user.home") + "/workspace/java-projects/target/output";
+        FileOutputFormat.setOutputPath(conf, new Path(destination));
 
+        JobClient.runJob(conf);
+    }
 }
